c2f5fb1f16cd09303fa7d083b5d8a10f1a9034ed
= <ledung2020604439@gmail.com>
2021-10-20 23:26:04 +0700

    BTVN_b1

---
+++ btvn_tuan1/bai1.py
@@ -0,0 +1,5 @@
+import math
+print("nhập bán kính hình tròn r:")
+r=int(input())
+print("Diện tích hình tròn có bán kinh r là:",math.pi*r*r)
+print("Chu vi hình tròn có bán kinh r là:",math.pi*r*2)
---
+++ btvn_tuan1/bai2.py
@@ -0,0 +1,4 @@
+import math
+print("Nhap x:")
+x=int(input())
+print("Gía trị của F(x) là:",(x*x+math.exp(x)+math.sin(x))/math.sqrt(x*x+1))
